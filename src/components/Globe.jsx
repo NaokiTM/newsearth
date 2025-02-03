68f1e910737d8260f8902a0e4ddb3bbf13824448
NaokiTM <ntsome21@gmail.com>
2025-02-03 15:38:11 +0000

    added earth texture to globe.

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useTexture } from '@react-three/drei';
 
 const apiKey = import.meta.env.REACT_APP_NEWS_API_KEY; 
 const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
@@ -10,10 +10,13 @@ fetch(url)
   .catch(error => console.error('Error fetching news:', error));
 
 const Sphere = () => {
+    const earthTexture = useTexture('/earth.jpg')
+
     return (
-        <mesh scale={0.75} rotation={[1, 1, 0]}>
+        <mesh scale={0.75} rotation={[0, Math.PI / 2, 0]}>
             <sphereGeometry args={[4, 64, 64]} />
-            <meshStandardMaterial color="royalblue" />
+            {/* <meshStandardMaterial color="royalblue" /> */}
+            <meshStandardMaterial map={earthTexture} />
         </mesh>
     );
 };
@@ -21,13 +24,15 @@ const Sphere = () => {
 const Globe = () => {
   return (
     <>
-    <div className='w-[400px] h-[400px] border-2'>
+    <div className='w-[400px] h-[400px]'>
         <Canvas classname = 'w-full h-full'camera={{position: [0,0,5], fov: 75}}>
-            <ambientLight intensity={0.5} />
+            <ambientLight intensity={4} />
             <directionalLight position={[5, 5, 5]} intensity={1} />
-
             <Sphere />
-            <OrbitControls enableZoom={false}/>
+            <OrbitControls enableZoom={false}
+            // maxPolarAngle={Math.PI / 2} 
+            // minPolarAngle={Math.PI / 2} 
+            />
         </Canvas>
     </div>
     </>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 const Title = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-        <div className="text-5xl">The Globe - News</div>
+    <div className="flex flex-col items-center justify-center pb-8 pt-2 italic font-bold tracking-tight">
+        <div className="text-5xl ">The Globe - News</div>
         <div>The earths news, delivered to you—anytime, anywhere!</div>
     </div>
   )
